6bca03d7f09ab62e7960978cc614db0d3426dc89
Johannes Neumeier <johannes.neumeier@gmail.com>
2012-09-20 10:04:31 +0300

    added support for several different checkboxes in one document; options added

--- js/jquery.styled-checkboxes/jquery.styled-checkboxes-0.1.js
+++ js/jquery.styled-checkboxes/jquery.styled-checkboxes-0.1.js
@@ -1,42 +1,67 @@
 (function ($) {
     
-    var wrapperSelector = '';
+    // do not change those defaults here, instead, pass them in a options
+    // object when creating the styled checkboxes
     var defaults = {
         'checkedClass' : 'checked',
+        'tick' : false,
+        'tickClass': 'styled-checkboxes-tick',
+        'tickContent': '&nbsp;',
         'wrapper' : 'div',
-        'wrapperClass' : 'styled-checkboxes'        
+        'wrapperClass' : 'styled-checkboxes'
     };
+    
+    // after creation, this object holds all the settings
     var settings = {};
     
+    // internal helpers
+    var tickSelector = '';
+    var wrapperSelector = '';
+    
+    // collection of methods the plugin can call like so:
+    // $(selector).styledCheckboxes('methodName');
+    // 
+    // call without method name results in calling 'init'
     var methods = {       
            
-        'init': function (options) {            
-            settings = $.extend(defaults, options);            
+        // setup and transform the checkboxes
+        'init': function () {     
             wrapperSelector = settings.wrapper + '.' + settings.wrapperClass;
-            
-            // attach name spaced click event to wrapper or labels containing
-            // a wrapper element
-            $(document).on('click.styleCheckboxes', 
-                '.styled-checkboxes, label:has(.styled-checkboxes)', 
-                methods.onClick);
-            
+            if (settings.tick !== false) {
+                tickSelector = settings.tick + '.' + settings.tickClass;
+            }
+                        
             // return chainable jQuery selection
             return this.map(function () {         
                 var $this = $(this);
-                var checked = $this.attr('checked') === 'checked' 
+                var checked = $this.prop('checked') === true 
                     ? settings.checkedClass : '';                
+                
                 
                 // wrap the checkbox
                 $this.wrap('<' + settings.wrapper + ' class="' 
                     + settings.wrapperClass 
-                    + ' ' + checked + '"/>');                
+                    + ' ' + checked + '"/>');
+                         
+                // attach        
+                $this.parent(wrapperSelector).bind('click.styledCheckboxes', 
+                    _methods.onClick);
+                if ($this.parent(wrapperSelector).parent('label').length > 0) {
+                    $this.parent(wrapperSelector).parent('label')
+                        .bind('click.styledCheckboxes', _methods.onClick);
+                }
                 
                 // return the wrapping div instead of the now invisible checkbox
-                return $(this).parent(wrapperSelector)[0];
+                return $this.parent(wrapperSelector)[0];
             });
-        },
+        }    
+    };
+    
+    // private methods that can't be called form the outside
+    var _methods = {
         
         'onClick': function (event) {
+            console.log('onClick');
             
             var $this = $(this).is('label') ? 
                 $(this).find(wrapperSelector) : $(this);
@@ -50,27 +75,38 @@
                 }
             }
             
+            console.log('contains checked checkbox: ' + $this.has('input[type="checkbox"]:checked').length);
+            
             // add or remove visual 'checked' classes 
             if ($this.has('input[type="checkbox"]:checked').length === 1) {
                 $this.removeClass(settings.checkedClass)
                     .find('input[type="checkbox"]').removeAttr('checked');
+                if (settings.tick !== false) {
+                    $this.find(tickSelector).remove();
+                }
             } else {
                 $this.addClass(settings.checkedClass)
+                    .append('<' + settings.tick + ' class="' + settings.tickSelector
+                        + '">' + settings.tickContent + '</' + settings.tick + '>')
                     .find('input[type="checkbox"]').attr('checked', 'checked');
             }
         }
         
     };
     
-    $.fn.styledCheckboxes = function (method) {
-    
+    $.fn.styledCheckboxes = function (method) {    
+        var options = {}; 
+           
+        if (typeof arguments[0] === 'object') {
+            settings = $.extend(defaults, arguments[0]);
+        } else {
+            settings = defaults;
+        }
+                
         if (methods.method) {
             //
-        } else if (typeof options === 'object') {
-            return methods.init.apply(this, options);
         } else {
-            console.log('init without options');
-            return methods.init.apply(this, null);
+            return methods.init.apply(this);
         }        
     }
 
